94bfd42fb6b94d2cd7f61c749755ff8f87df461b
Ivan <MacBookPro@192.168.0.26>
2023-03-06 01:19:25 +0300

    addid DZTask04

---
+++ DZTask04/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая 
+// принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Максимальное из трёх чисел.");
+
+Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if(numberA > max)
+{
+    max = numberA;
+}
+
+if(numberB >max)
+{
+    max = numberB;
+}
+
+if(numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine($"Максимальным числов из трёх являтся: {max}");
