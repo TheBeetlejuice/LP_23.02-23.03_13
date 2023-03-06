4a74379ddab06b0f672bbd7072d769506be19512
Ivan <MacBookPro@192.168.0.26>
2023-03-06 12:02:02 +0300

    addid DZTask08

--- DZTask08/Program.cs
+++ DZTask08/Program.cs
@@ -4,19 +4,17 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
-// */
-
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int evenNumber = 2;
+int value = 2;
 
 if(number > 1)
 {
-    while(evenNumber <= number)
+    while(value <= number)
     {
-        Console.Write(evenNumber + " ");
-        evenNumber = evenNumber + 2;
+        Console.Write(value + " ");
+        value = value + 2;
     }
 }
