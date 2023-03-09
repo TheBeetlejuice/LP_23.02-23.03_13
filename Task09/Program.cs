b887443cb20bbec501c1fb3562810f50c957b029
Ivan <MacBookPro@192.168.0.11>
2023-03-09 14:17:01 +0300

    Task 09 метод

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -8,6 +8,7 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+
 // int firstDigit = number / 10;
 // int secondDigit = number % 10;
 
