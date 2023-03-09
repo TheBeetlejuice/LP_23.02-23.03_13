c9dd805f1227f0121adbf113be6dfdb6f4d1b07f
Ivan <MacBookPro@192.168.0.11>
2023-03-09 22:06:51 +0300

    Task 16 метод bool

---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает 
+// на вход два числа и проверяет, является ли 
+// одно число квадратом другого.
+  
+// 5, 25 -> да 
+// -4, 16 -> да 
+// 25, 5 -> да
+
+Console.Write("Введите первое чсло: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе чсло: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SqrtNumbers(firstNumber, secondNumber) ? "Да" : "Нет");
+
+bool SqrtNumbers(int firstNum, int secondNum)
+{
+    return firstNum * firstNum == secondNum || secondNum * secondNum == firstNum;
+}
