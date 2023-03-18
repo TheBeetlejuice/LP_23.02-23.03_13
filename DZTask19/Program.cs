3100728795949acc5821b4cbef87afa5455303d1
Ivan <MacBookPro@192.168.0.11>
2023-03-18 19:03:28 +0300

    addid DZTask 19 полиндром

---
+++ DZTask19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+// string rev = string.Concat(value.Reverse());
+// Console.WriteLine(value == rev ?
+//                     ($"{value}, является палиндромом.") :
+//                     ($"{value}, не является палиндромом."));
+
+void Polindrom(string num)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"Число {num} является палиндромом.");
+    }
+    else Console.WriteLine($"Число {num} не является палиндромом.");
+}
+
+if (number.Length == 5)
+{
+    Polindrom(number);
+}
+else Console.WriteLine($"Число не пятизначное.");
