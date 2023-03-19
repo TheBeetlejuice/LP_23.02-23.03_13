e2b811c381cf153bc97559daa675b085288a66cb
Ivan <MacBookPro@192.168.0.11>
2023-03-19 23:05:21 +0300

    addid DZTask04 максимальное из этих чисел

--- DZTask04/Program.cs
+++ DZTask04/Program.cs
@@ -6,10 +6,8 @@ Console.WriteLine("Максимальное из трёх чисел.");
 
 Console.WriteLine("Введите первое число:");
 int numberA = Convert.ToInt32(Console.ReadLine());
-
 Console.WriteLine("Введите второе число:");
 int numberB = Convert.ToInt32(Console.ReadLine());
-
 Console.WriteLine("Введите третье число:");
 int numberC = Convert.ToInt32(Console.ReadLine());
 
