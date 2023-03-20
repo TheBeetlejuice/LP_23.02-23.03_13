a2beea6e188802066ceb8b0828320949a9e7341a
Your Name <you@example.com>
2023-03-20 22:25:01 +0300

    addid Task28 произведение чисел от 1 до N

---
+++ Task28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите проограмму, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int workNumbers = WorkNumbers(number); //вызов метода
+Console.WriteLine($"Произведение чисел от 1 до {number} = { workNumbers}");
+
+int WorkNumbers(int num)
+{
+    int work = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        work *= i; //sum = sum + i;
+    }
+    return work;
+}
