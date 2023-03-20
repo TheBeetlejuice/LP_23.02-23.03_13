81b191bcbfd77ecf996c8f762ec21a14b773b882
Your Name <you@example.com>
2023-03-21 01:05:18 +0300

    addid DZTask25 степень

---
+++ DZTask25/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит 
+// число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+// int expo = Expo(numberA, numberB);
+// Console.WriteLine($"Первое число {numberA} в степени второго числа {numberB} = " + expo);
+
+// int Expo(int numA, int numB)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= numB; i++)
+//     {
+//         result = result * numA;
+//     }
+//     return result;
+// }
+
+
+int result = 1;
+if (numberB < 1)
+    Console.Write($"Число B должно быть натуральным.");
+else
+{
+    while (numberB != 0)
+    {
+        result = result * numberA;
+        numberB = numberB - 1;
+    }
+    Console.WriteLine($"Число A в натурального степени числа B -> " + result);
+}
