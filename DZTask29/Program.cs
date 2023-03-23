75d8dc2a8faec682149a005907c919d6ef76c18e
Your Name <you@example.com>
2023-03-23 23:56:10 +0300

    addid DZTask29 вывод массива на экран, запятую не знаю как убрать последнюю

---
+++ DZTask29/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая 
+// задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+            void FillArray(int[] arr)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(0, 100);
+                }
+            }
+
+            void PrintArray(int[] arr)
+            {
+                int count = arr.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write($"{arr[i]}, ");
+                }
+            }
+
+            void PrintFin(int[] arr)
+            {
+                Console.Write($"-> [");
+                int count = arr.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write($"{arr[i]},");
+                }
+                Console.Write($"]");
+            }
+
+            FillArray(array);
+            PrintArray(array);
+            PrintFin(array);
