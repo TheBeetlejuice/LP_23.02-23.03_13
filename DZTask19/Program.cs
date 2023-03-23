d1e119e4f9286df5a08a76d7225a55d3b4d7af5e
Your Name <you@example.com>
2023-03-23 18:53:27 +0300

    addid DZTask19 полиндром

--- DZTask19/Program.cs
+++ DZTask19/Program.cs
@@ -6,24 +6,36 @@
 // 23432 -> да
 
 Console.Write("Введите пятизначное число: ");
-string number = Console.ReadLine();
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000)
+{
+    int pal = 0, num = number;
+    while (number != 0)
+    {
+        pal = pal * 10 + number % 10;
+        number /= 10;
+    }
+    Console.WriteLine(num == pal ? "Палиндром" : "Не палиндром");
+}
+else Console.WriteLine ("Введите пятизначное число! ");
 
 // string rev = string.Concat(value.Reverse());
 // Console.WriteLine(value == rev ?
 //                     ($"{value}, является палиндромом.") :
 //                     ($"{value}, не является палиндромом."));
 
-void Polindrom(string num)
-{
-    if (num[0] == num[4] && num[1] == num[3])
-    {
-        Console.WriteLine($"Число {num} является палиндромом.");
-    }
-    else Console.WriteLine($"Число {num} не является палиндромом.");
-}
+// void Polindrom(string num)
+// {
+//     if (num[0] == num[4] && num[1] == num[3])
+//     {
+//         Console.WriteLine($"Число {num} является палиндромом.");
+//     }
+//     else Console.WriteLine($"Число {num} не является палиндромом.");
+// }
 
-if (number.Length == 5)
-{
-    Polindrom(number);
-}
-else Console.WriteLine($"Число не пятизначное.");
+// if (number.Length == 5)
+// {
+//     Polindrom(number);
+// }
+// else Console.WriteLine($"Число не пятизначное.");
