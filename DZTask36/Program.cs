fcb97f795864f1e1bd6ab3e45cb6fb8bb5e970d6
Your Name <you@example.com>
2023-04-02 16:03:06 +0300

    addid DZTask36 Сумма элементов массива, стоящих на нечётных позициях

---
+++ DZTask36/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = CreateArrayRndInt(4, -100, 100);
+PrintArray(array);
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("Сумма элементов массива, стоящих на нечётных позициях:");
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("] -> ");
+}
+
+int sumOddNmberCount = 0;
+
+for (int i = 1; i < array.Length; i += 2)
+{
+    sumOddNmberCount += array[i];
+}
+Console.WriteLine($"{sumOddNmberCount}");
