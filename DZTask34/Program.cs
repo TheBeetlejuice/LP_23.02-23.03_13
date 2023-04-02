0e4f7eee5d6855252e10cea6173de2c61676a62f
Your Name <you@example.com>
2023-04-02 13:49:12 +0300

    addid DZTask34 четные элементы массива

---
+++ DZTask34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, которая покажет 
+// количество чётных чисел в массиве.
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int eventNumbersCount = 0;
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    eventNumbersCount++;
+}
+
+Console.WriteLine($"Количество чётных чисел в массиве: {eventNumbersCount} ");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Элементы массива: [ ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine("]");
+}
