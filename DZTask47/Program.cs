f230885f2200eab0b8e61ea3cfe88d6efa7a66bf
Your Name <you@example.com>
2023-04-07 22:49:01 +0300

    addid DZTask47 массив из вещественных чисел

---
+++ DZTask47/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+
+double[,] matrix = new double[3, 4];
+Random rnd = new Random();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.NextDouble() * 10;
+        Console.Write("{0,6:F2}", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
